80025514cde7881e5262a0cdebaed0f2067b4bdb
GauravTheBeginner <gaurav8861r@gmail.com>
2025-01-12 23:42:25 +0530

    part4

--- src/components/notes/NoteCard.tsx
+++ src/components/notes/NoteCard.tsx
@@ -6,7 +6,7 @@ import {
   Trash2, 
   X, 
   Check, 
-  MoreVertical,
+  MoreHorizontal,
 } from 'lucide-react';
 import {
   DropdownMenu,
@@ -146,10 +146,10 @@ export function NoteCard({ note, onDelete, onUpdate }: NoteCardProps) {
                 <DropdownMenuTrigger asChild>
                   <Button
                     variant="ghost"
-                    size="icon"
-                    className="h-8 w-8 opacity-0 group-hover:opacity-100 transition-opacity"
+                    size="sm"
+                   
                   >
-                    <MoreVertical className="h-4 w-4" />
+                    <MoreHorizontal height={15} width={15} color="white"/>
                   </Button>
                 </DropdownMenuTrigger>
                 <DropdownMenuContent align="end">
